5e5bee5ee49711c863761b9ca88dccdef57e3d85
Romel Perez <ronelprhone@gmail.com>
2022-03-18 02:11:09 -0500

    fix: constant LOREM_IPSUM

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,4 @@ sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad
 minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
 commodo consequat duis aute irure dolor in reprehenderit in voluptate velit
 esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat
-non proident sunt in culpa qui officia deserunt mollit anim id est`.split(' ');
+non proident sunt in culpa qui officia deserunt mollit anim id est`;
--- src/createRandomWords/createRandomWords.ts
+++ src/createRandomWords/createRandomWords.ts
@@ -1,9 +1,11 @@
 import { LOREM_IPSUM } from '../constants';
 
-const createRandomWords = (length: number = 3): string =>
-  Array(length)
+const createRandomWords = (length: number = 3): string => {
+  const words = LOREM_IPSUM.split(' ');
+  return Array(length)
     .fill(0)
-    .map(() => LOREM_IPSUM[Math.round(Math.random() * LOREM_IPSUM.length - 1)])
+    .map(() => words[Math.round(Math.random() * (words.length - 1))])
     .join(' ');
+};
 
 export { createRandomWords };
