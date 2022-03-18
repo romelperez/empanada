89e700dc3147716601606d5f1ccec134e01370fd
Romel Perez <ronelprhone@gmail.com>
2022-03-18 01:52:33 -0500

    test: createRandomWords

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
-export const LOREM_IPSUM = (`lorem ipsum dolor sit amet consectetur adipiscing elit
+export const LOREM_IPSUM =
+  `lorem ipsum dolor sit amet consectetur adipiscing elit
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad
 minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
 commodo consequat duis aute irure dolor in reprehenderit in voluptate velit
 esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat
-non proident sunt in culpa qui officia deserunt mollit anim id est`).split(' ');
+non proident sunt in culpa qui officia deserunt mollit anim id est`.split(' ');
--- src/createRandomWords/createRandomWords.test.ts
+++ src/createRandomWords/createRandomWords.test.ts
@@ -6,3 +6,17 @@ test('Should create 3 words by default', () => {
   const received = createRandomWords();
   expect(received.split(' ')).toHaveLength(3);
 });
+
+test('Should create provided length of words', () => {
+  const received = createRandomWords(5);
+  expect(received.split(' ')).toHaveLength(5);
+});
+
+test('Should create non-empty strings as words', () => {
+  const received = createRandomWords(7);
+  const validWords = received
+    .split(' ')
+    .map((w) => w.trim())
+    .filter(Boolean);
+  expect(validWords).toHaveLength(7);
+});
