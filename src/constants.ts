93d3e663aa7424ebd6d87b65d7c61253663016db
Romel Perez <ronelprhone@gmail.com>
2022-03-18 02:13:56 -0500

    fix: lint

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
-export const LOREM_IPSUM =
-  `lorem ipsum dolor sit amet consectetur adipiscing elit
+export const LOREM_IPSUM = `lorem ipsum dolor sit amet consectetur adipiscing elit
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad
 minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
 commodo consequat duis aute irure dolor in reprehenderit in voluptate velit
